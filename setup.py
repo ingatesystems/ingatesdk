0e6234439d13718cc592948df771df0eceda8929
Erik Andersson <erik@ingate.com>
2018-10-19 02:10:12 +0200

    Bump version to 1.0.5

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except:
 
 setuptools.setup(
     name=PKGNAME,
-    version='1.0.4',
+    version='1.0.5',
     author='Ingate Systems AB',
     author_email='fuegodev@ingate.com',
     description='Ingate Python SDK',
